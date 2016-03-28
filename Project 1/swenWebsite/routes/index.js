1b55443bd1a2728be916a699a0d2e8f6536e3f0d
Jovan Bogoievski <nzjovi@gmail.com>
2016-03-28 17:32:31 +1300

    Added basic search functionality

--- Project 1/swenWebsite/routes/index.js
+++ Project 1/swenWebsite/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var cheerio = require('cheerio');
+
+var basex = require('basex');
+var client = new basex.Session("localhost", 1984, "admin", "admin");
+client.execute("open Colenso");
+
+function getPlace(){
+	client.execute("XQUERY declare namespace tei= 'http://www.tei-c.org/ns/1.0'; " +
+	"//tei:name[@type = 'place' and position() = 1 and . = 'Manawarakau']",
+	function(err,res){ 
+		if(!err){ 
+			//console.log(res.result)
+			return res.result
+		}
+	} )
+}
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Colenso Project' });
+router.get('/', function(req, res, next) {	
+	res.render('index', { title: 'Colenso Project'});
 });
+
+router.get('/search', function(req, res) {
+	var result = "";
+	client.execute("XQUERY declare namespace tei= 'http://www.tei-c.org/ns/1.0'; " +
+	"//tei:name[@type = 'place' and position() = 1 and . = '"+ req.query.searchString +"']",
+	function(error,result){ 
+		if(!error){ 
+			res.render('search', { title: 'Colenso Project', content: result.result });
+			console.log(result.result)
+		}
+	} )
+});
+
 
 module.exports = router;
