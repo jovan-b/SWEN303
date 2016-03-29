3fef8cc48709410b28034d55c90426f9da36f038
Jovan Bogoievski <nzjovi@gmail.com>
2016-03-29 23:03:26 +1300

    Added support for basic XQuery search and support of logical operators

--- Project 1/swenWebsite/routes/index.js
+++ Project 1/swenWebsite/routes/index.js
@@ -8,16 +8,31 @@ var basex = require('basex');
 var client = new basex.Session("localhost", 1984, "admin", "admin");
 client.execute("open Colenso");
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {	
 	res.render('index', { title: 'Colenso Project | Home'});
 });
 
 router.get('/search', function(req, res) {
-	var searchInput = req.query.searchString;
+	var searchString = req.query.searchString;
+	var searchMarkup = req.query.searchMarkup;
 	var searchResults = [];
+	var query;
 	
-	client.execute(tei + "//TEI[. contains text '"+ searchInput +"']",
+	if(searchString.length != 0){
+		var logicalString = searchString
+			.replace(" AND ", '\' ftand \'')
+			.replace(" OR ", '\' ftor \'')
+			.replace(" NOT ", '\' ftnot \'');
+			
+		query = tei + "//TEI[. contains text '"+ logicalString +"' using wildcards]";
+	}
+	else{
+		query = tei + searchMarkup;
+	}
+	
+	client.execute(query,
 	function(error,result){ 
 		if(!error){ 
 			var data = cheerio.load(result.result,{
@@ -32,7 +47,7 @@ router.get('/search', function(req, res) {
 				});
 			});
 			res.render('search', { title: 'Colenso Project | Search Results', results: searchResults,
-									search: searchInput});
+									searchString: searchString, searchMarkup: searchMarkup});
 		}
 		else{
 			console.log(error);
